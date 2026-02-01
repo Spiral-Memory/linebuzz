d57514265ec8260294976c5287592138cb28e9b6
Zishan Ahmad <zishan.barun@gmail.com>
2026-02-02 00:54:28 +0530

    add diff if not exact match
    Signed-off-by: Zishan Ahmad <zishan.barun@gmail.com>

--- src/webview/components/chat/MessageAttachment/SnippetAttachment.tsx
+++ src/webview/components/chat/MessageAttachment/SnippetAttachment.tsx
@@ -120,13 +120,14 @@ export const SnippetAttachment = ({ snippet, onNavigate }: SnippetAttachmentProp
                             width="14"
                             height="14"
                             viewBox="0 0 24 24"
-                            fill="none"
+                            fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                             onClick={handleDiffClick}
                             style={{ color: '#dea841', cursor: 'pointer' }}
                         >
                             <title>Show Diff (Snippet modified)</title>
-                            <path d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M10.146 3.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.293 7H6.5A2.5 2.5 0 0 0 4 9.5v5a.5.5 0 0 1-1 0v-5A3.5 3.5 0 0 1 6.5 6h5.793l-2.147-2.146a.5.5 0 0 1 0-.708z" />
+                            <path d="M13.854 20.854a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708.708L11.707 17H17.5a2.5 2.5 0 0 0 2.5-2.5v-5a.5.5 0 0 1 1 0v5a3.5 3.5 0 0 1-3.5 3.5h-5.793l2.147 2.146a.5.5 0 0 1 0 .708z" />
                         </svg>
                     )}
                     <span onClick={handleClick} style={{ cursor: 'pointer' }} title="Jump to Source">
